07ceec18e232c2da22b757968768123266b1e76b
Taimoor Najam <taimoor_najam@yahoo.com>
2024-02-13 08:54:58 -0500

    tnajam: Add navigation components and click listeners

--- app/src/main/java/com/udacity/shoestore/fragments/ShoeListingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/fragments/ShoeListingFragment.kt
@@ -4,21 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentShoeListingBinding
 
 class ShoeListingFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_listing, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val binding: FragmentShoeListingBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_shoe_listing, container, false)
+        binding.fabShoeListingAddShoe.setOnClickListener { view: View ->
+            view.findNavController().navigate(ShoeListingFragmentDirections.actionShoeListingFragmentToShoeDetailFragment())
+        }
+        return binding.root
     }
 
 }
